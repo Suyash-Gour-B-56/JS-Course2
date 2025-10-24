b2f61e8f993375453497d04989a8102c67bf191c
Suyash Gour A4-66 <139151852+Suyash-Gour-B-56@users.noreply.github.com>
2025-10-25 02:06:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
+// src/main.jsx
 // Author: Suyash Gour | Date: 25 October 2025
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import App from "./App";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
