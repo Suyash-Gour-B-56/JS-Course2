255b58e03abcddc8da7089f334fd4f54fd81952c
Suyash Gour A4-66 <139151852+Suyash-Gour-B-56@users.noreply.github.com>
2025-10-25 01:56:28 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+// Author: Suyash Gour | Date: 25 October 2025
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/JS-Course2/' // needed for GitHub Pages
+});
