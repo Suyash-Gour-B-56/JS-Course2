b0180188181a8f7a8405bef8ee6d848e89078e72
Suyash Gour A4-66 <139151852+Suyash-Gour-B-56@users.noreply.github.com>
2025-10-25 02:08:26 +0530

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+// src/pages/About.jsx
+// Author: Suyash Gour | Date: 25 October 2025
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We provide a variety of houseplants to beautify your home.</p>
+    </div>
+  );
+}
+
+export default About;
