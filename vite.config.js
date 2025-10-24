75c5510f6fe2633095dad5bb37d85b1a334834bd
Suyash Gour A4-66 <139151852+Suyash-Gour-B-56@users.noreply.github.com>
2025-10-25 02:19:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
 // Author: Suyash Gour | Date: 25 October 2025
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// ✅ Important: set the base path to your repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/JS-Course2/' // Required for GitHub Pages deployment
-});
+  base: '/JS-Course2/'
+})
